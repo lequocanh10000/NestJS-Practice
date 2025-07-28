2c64233011ad68a64c3a60103fda702e48ca968f
Tomoriu Akarin <lequocanh10000@github.com>
2025-07-28 16:32:11 +0700

    feat bai3: add some api update, delete, get

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Put, Delete, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 
@@ -9,5 +9,25 @@ export class ProjectController {
   @Post('create')
   async createProject(@Body() createProjectDto: CreateProjectDto) {
     return await this.projectService.create(createProjectDto);
+  }
+
+  @Get('all')
+  async getAllProjects() {
+    return await this.projectService.findAll();
+  }
+
+  @Get('one/:id')
+  async getProjectById(@Param('id', ParseIntPipe) id: number) {
+    return await this.projectService.findById(id);
+  }
+
+  @Put('update/:id')
+  async updateProject(@Param('id', ParseIntPipe) id: number, @Body() updateProjectDto: CreateProjectDto) {
+    return await this.projectService.update(id, updateProjectDto);
+  }
+
+  @Delete('delete/:id')
+  async deleteProject(@Param('id', ParseIntPipe) id: number) {
+    return await this.projectService.delete(id);
   }
 }
