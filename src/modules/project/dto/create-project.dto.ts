bfdac31ece119dfdd078a79561fa14690f9e2850
Tomoriu Akarin <lequocanh10000@github.com>
2025-07-30 16:31:48 +0700

    feat: add dto files

--- src/modules/project/dto/create-project.dto.ts
+++ src/modules/project/dto/create-project.dto.ts
@@ -1,8 +1,31 @@
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, Length, Matches } from "class-validator";
+import { IsAfter } from "sequelize-typescript";
+import { ProjectEnum } from "src/models/project.model";
+
 export class CreateProjectDto {
+    @IsString()
+    @IsNotEmpty()
+    // @Length(8, 10, {message: "Have from 8-10 chars"})
+    // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/, { message: 'At least an upper, a lower and a digit',})
     name: string;
-    category?: string;
+
+    @IsOptional()
+    @IsEnum(ProjectEnum, {message: "Must be in the enum"})
+    category?: ProjectEnum;
+
+    @IsOptional()
+    @IsNumber()
     projectedSpend?: number;
+
+    @IsOptional()
+    @IsNumber()
     projectedVariance?: number;
-    projectStartedAt?: string;
-    projectEndedAt?: string;
+
+    @IsOptional()
+    @IsDate()
+    projectedStartedAt?: Date;
+
+    @IsOptional()
+    @IsDate()
+    projectedEndedAt?: Date;
 }
