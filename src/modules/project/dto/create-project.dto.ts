4d5f84d37110e5b7868debd43eae81e1f00ddb8c
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-01 17:25:41 +0700

    feat: fix file

--- src/modules/project/dto/create-project.dto.ts
+++ src/modules/project/dto/create-project.dto.ts
@@ -1,12 +1,11 @@
-import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString, Length, Matches } from "class-validator";
+import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString} from "class-validator";
 import { IsAfter } from "sequelize-typescript";
 import { ProjectEnum } from "src/models/project.model";
 
 export class CreateProjectDto {
     @IsString()
     @IsNotEmpty()
-    // @Length(8, 10, {message: "Have from 8-10 chars"})
-    // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/, { message: 'At least an upper, a lower and a digit',})
+   
     name: string;
 
     @IsOptional()
@@ -23,9 +22,11 @@ export class CreateProjectDto {
 
     @IsOptional()
     @IsDate()
+    // @IsAfter(new Date().toISOString())
     projectedStartedAt?: Date;
 
     @IsOptional()
     @IsDate()
+    // @IsAfter(started => started.getStartProjectDate())
     projectedEndedAt?: Date;
 }
