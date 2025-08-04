d9ea483daad0dfd86b7e51505e4f46bcc8aea9e7
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-04 09:54:49 +0700

    feat bai 2+3: create project-dto file

--- src/modules/project/dto/create-project.dto.ts
+++ src/modules/project/dto/create-project.dto.ts
@@ -2,34 +2,13 @@
 
 
 import { IsDate, IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString} from "class-validator";
-import { IsAfter } from "sequelize-typescript";
 import { ProjectEnum } from "src/models/project.model";
 
 export class CreateProjectDto {
-    @IsString()
-    @IsNotEmpty()
-   
     name: string;
-
-    @IsOptional()
-    @IsEnum(ProjectEnum, {message: "Must be in the enum"})
     category?: ProjectEnum;
-
-    @IsOptional()
-    @IsNumber()
     projectedSpend?: number;
-
-    @IsOptional()
-    @IsNumber()
     projectedVariance?: number;
-
-    @IsOptional()
-    @IsDate()
-    // @IsAfter(new Date().toISOString())
     projectedStartedAt?: Date;
-
-    @IsOptional()
-    @IsDate()
-    // @IsAfter(started => started.getStartProjectDate())
     projectedEndedAt?: Date;
 }
