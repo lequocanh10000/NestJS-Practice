ffa994622111625aadadb9deb431f964b5c6e168
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-04 10:06:17 +0700

    feat bai5: write logic find projects

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from 'src/models';
-import { CreateUserDto } from './dto/create-user.dto';
-import * as bcrypt from 'bcryptjs'
+
 
 @Injectable()
 export class UserService {
@@ -10,54 +9,8 @@ export class UserService {
         @InjectModel(User) private readonly userModel: typeof User
     ) {}
 
-    async findByEmail(email: string) {
-        return this.userModel.findOne(
-            {
-                where: { email},
-                raw: true
-            }
-        )
-    }
-
-    async register(createUserDto: CreateUserDto) {
-        const user = await this.findByEmail(createUserDto.email);
-
-        if(user) {
-            throw new Error('User has been registered');
-        }
-
-        const hashedPassword = bcrypt.hashSync(createUserDto.password, 10);
-        const payload = {
-            ...createUserDto,
-            password: hashedPassword
-        }
-
-        await this.userModel.create(payload as any);
-
-        return { 
-            message: 'Registered successfully',
-        };
-    }
-
-    async login(email: string, password) {
-        const user = await this.findByEmail(email)
-        if(!user) {
-            throw new Error('User has not been registered');
-        }
-
-        const isCorrectPassword = bcrypt.compareSync(password, user.password);
-        if(!isCorrectPassword){
-            throw new Error('Wrong password.');
-        }
-
-        const { password: _, ...rest} = user;
-
-        return {
-            message: 'LOGGEN IN',
-            data: rest
-        }
-    }
-
+    
+    // Write logic findProject
     async findUserProjects(id: number) {
         return await this.userModel.findOne({
             where: { id},
