de5795ef7291157005700318a878c43b36ba1f09
Tomoriu Akarin <lequocanh10000@github.com>
2025-08-08 10:44:15 +0700

    feat: implement interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpectionFilters } from './filters/expection-filters';
+import { TransformInterceptor } from './interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3002;
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new ExpectionFilters);
   console.log(`Server starts on port ${port}`);
   await app.listen(port);
